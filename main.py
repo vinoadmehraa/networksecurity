ac1419dc8008d2ff8e0edc3dafc47d75bbdbdce7
“Vinoad <vinoad.mehraa@gmail.com>
2025-04-29 14:48:58 -0700

    Data Validation Added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
-#from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_validation import DataValidation
 #from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig #,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig ,DataValidationConfig #,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 #from networksecurity.components.model_trainer import ModelTrainer
@@ -21,14 +21,14 @@ if __name__=='__main__':
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
+        print("\n",dataingestionartifact)
         
-        #data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        #data_validation=DataValidation(dataingestionartifact,data_validation_config)
-        #logging.info("Initiate the data Validation")
-        #data_validation_artifact=data_validation.initiate_data_validation()
-        #logging.info("data Validation Completed")
-        #print(data_validation_artifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print("\n",data_validation_artifact)
         
         #data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
         #logging.info("data Transformation started")
